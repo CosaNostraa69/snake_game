eb007bd33778b4fe0d2adf1eda3f9f7cf775c505
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-03-07 18:24:06 +0100

    Add random food types and their effects

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ game_window = pygame.display.set_mode((screen_width, screen_height))
 # Couleurs
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
 
 # Fréquence d'actualisation
 clock = pygame.time.Clock()
@@ -22,6 +20,15 @@ snake_speed = 15
 # Initialisation du serpent
 snake_pos = [100, 50]
 snake_body = [[100, 50], [90, 50], [80, 50]]
+
+# Types de nourriture avec leurs couleurs et effets
+food_types = {
+    'speed_up': {'color': pygame.Color(255, 50, 50), 'speed_change': 5},
+    'slow_down': {'color': pygame.Color(50, 255, 50), 'speed_change': -3}
+}
+
+# Choisir un type de nourriture aléatoirement et initialiser sa position
+food_type = random.choice(list(food_types.keys()))
 food_pos = [random.randrange(1, (screen_width//10)) * 10, random.randrange(1, (screen_height//10)) * 10]
 food_spawn = True
 
@@ -58,18 +65,23 @@ while True:
     snake_body.insert(0, list(snake_pos))
     if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
         food_spawn = False
+        # Appliquer l'effet de la nourriture
+        snake_speed += food_types[food_type]['speed_change']
+        # Assurer que la vitesse du serpent ne devient pas trop lente ou trop rapide
+        snake_speed = max(5, min(30, snake_speed))
     else:
         snake_body.pop()
 
     if not food_spawn:
+        food_type = random.choice(list(food_types.keys()))
         food_pos = [random.randrange(1, (screen_width//10)) * 10, random.randrange(1, (screen_height//10)) * 10]
     food_spawn = True
 
     # Affichage
     game_window.fill(black)
     for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+        pygame.draw.rect(game_window, white, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(game_window, food_types[food_type]['color'], pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
     # Vérification des collisions
     if snake_pos[0] < 0 or snake_pos[0] > screen_width-10 or snake_pos[1] < 0 or snake_pos[1] > screen_height-10:
